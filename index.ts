8e4a67c676945c54347f796ec36e1c8f3e7eec4b
Ðåvï åvêlïñð <daviavelino2007@gmail.com>
2025-05-27 07:36:54 -0300

    putting cors again

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import express from "express"
+import cors from "cors"
 import {route} from "./routes/router"
 
 const app = express()
 
 
+app.use(cors())
 app.use(express.json())
 app.use(route)
-
 
 
 app.listen(3000, () => {
