381cb6d5a5c21ca4cf43095412647cd65f763d78
Ðåvï åvêlïñð <daviavelino2007@gmail.com>
2025-05-27 07:21:20 -0300

    Removing cors

--- routes/router.ts
+++ routes/router.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import {CreateHello} from "../controller/CreateHello"
-import { GetCongrats } from "controller/GetCongrats";
+// import { GetCongrats } from "controller/GetCongrats";
 
 const route = Router()
 
 const hello = new CreateHello()
-const congrats = new GetCongrats()
+// const congrats = new GetCongrats()
 
 
 route.get('/', hello.handle)
-route.get('/get', congrats.handle)
+// route.get('/get', congrats.handle)
 
 
 
